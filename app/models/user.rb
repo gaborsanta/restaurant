37d95c7ab12fdca99a9a8f108262caedcd1c0e5c
Gabor Santa <Gabor.Santa@gmail.com>
2017-03-18 16:34:54 -0400

    took out User check from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,27 +12,6 @@ class User < ApplicationRecord
   #and return true when the User has the 'owner' role,
   #otherwise return false
 
-  def owner?
-    if @User.role === "owner"
-      #debugger
-      true
-    else
-      false
-    end
 
-  end
-
-  #Create a method on User named 'patron?' and return true when the User has the 'patron' role,
-  #otherwise return false
-
-  def patron?
-    if @User.role === "patron"
-      #debugger
-      true
-    else
-      false
-    end
-
-  end
 
 end
